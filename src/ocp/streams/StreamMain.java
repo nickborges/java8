a8d04cb2b180e038bedc3262e7d25c7f4f201281
nick.borges <nick.borges@cwi.com.br>
2019-12-09 09:32:32 -0300

    alteração nome do método

--- src/ocp/streams/StreamMain.java
+++ src/ocp/streams/StreamMain.java
@@ -7,17 +7,18 @@ import java.util.stream.Stream;
 
 public class StreamMain {
 
+    //stream from list
+    public static void streamFromList(){
+        List<String> l1 = Arrays.asList("a", "b", "c");
+        Stream<String> s4 = l1.stream(); //create a stream from a list
+        Stream<String> s5 = l1.parallelStream(); //creates a stream that is allowed to process elements in parallel, you can write code that uses parallelism before even learning what a thread is.
+    }
+
     //finite streams
     public static void finiteStream(){
         Stream<String> s1 = Stream.empty();
         Stream<String> s2 = Stream.of("1");
         Stream<Integer> s3 = Stream.of(1,2,3);
-    }
-
-    public static void teste(){
-        List<String> l1 = Arrays.asList("a", "b", "c");
-        Stream<String> s4 = l1.stream(); //criate a stream from a list
-        Stream<String> s5 = l1.parallelStream(); //creates a stream that is allowed to process elements in parallel, you can write code that uses parallelism before even learning what a thread is.
     }
 
     //infinite streams, but calls a limit() turns it on a finite stream
